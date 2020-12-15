c143e89f86bf033b7a5ca5c85085efb44a93a869
qtpham1998 <quynhtrangpham1998@gmail.com>
2020-12-15 15:15:31 +0000

    Day 15 clean

--- Day15/fifteen.py
+++ Day15/fifteen.py
@@ -1,21 +1,21 @@
+STARTING_NUMS = [6, 19, 0, 5, 7, 13, 1]
+LIMIT = 30000000
+
+
 def memory_game():
-  starting_numbers = [6, 19, 0, 5, 7, 13, 1]
   memory = {}
-  for i, num in enumerate(starting_numbers[:-1]):
+  for i, num in enumerate(STARTING_NUMS[:-1]):
     memory[num] = i + 1
   
-  last_num = starting_numbers[-1]
-  curr_turn = len(starting_numbers) + 1
-  while curr_turn <= 30000000:
+  last_num = STARTING_NUMS[-1]
+  for last_turn in range(len(STARTING_NUMS), LIMIT):
     if last_num not in memory.keys():
-      memory[last_num] = curr_turn - 1
+      memory[last_num] = last_turn
       last_num = 0
     else:
-      last_turn = curr_turn - 1
       num = last_turn - memory[last_num]
       memory[last_num] = last_turn
       last_num = num
-    curr_turn += 1
   print(f'The 30000000 number is {last_num}')
 
 
